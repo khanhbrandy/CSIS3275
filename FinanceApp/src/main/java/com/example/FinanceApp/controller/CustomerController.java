ac18ab844b77de1d604d614208d0dd83a5d70e9f
khanhbrandy <khanh.brandy@gmail.com>
2023-04-06 00:01:03 -0700

    rename response package

--- FinanceApp/src/main/java/com/example/FinanceApp/controller/CustomerController.java
+++ FinanceApp/src/main/java/com/example/FinanceApp/controller/CustomerController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.FinanceAp.response.MessageResponse;
+import com.example.FinanceApp.response.MessageResponse;
 import com.example.FinanceApp.model.Customer;
 import com.example.FinanceApp.model.CustomerRepository;
 import com.example.FinanceApp.request.CustomerLoginRequest;
