2140966c56887b950513e120cb4c6156f9c3ccbb
Juan Camilo <Juan.restrepo.belalcazar@gmail.com>
2023-04-09 13:56:52 -0700

    Goal Component Delete Goal implementation.

--- FrontEnd/vuefinance/src/components/FinancialGoal.vue
+++ FrontEnd/vuefinance/src/components/FinancialGoal.vue
@@ -1,45 +1,51 @@
 <template>
-    <div>
+    <header>
+        <h4> Create and track a Financial Goal </h4>
+    </header>
+    <section>
+        <div>
 
-        <h1> Create and track a Financial Goal </h1>
-
-        <form>
-            <div class="container">
+            <form>
+                <div class="container">
 
 
-                <label>Name</label>
-                <input type="text" v-model="GoalRequest.name" required>
+                    <label>Name</label>
+                    <input type="text" v-model="GoalRequest.name" required>
 
-                <label>Description</label>
-                <textarea placeholder="Add some description..." v-model="GoalRequest.description" required> </textarea>
+                    <label>Description</label>
+                    <textarea placeholder="Add some description..." v-model="GoalRequest.description" required> </textarea>
 
-                <label>Target Amount</label>
-                <input type="number" v-model="GoalRequest.amount" required>
+                    <label>Target Amount</label>
+                    <input type="number" v-model="GoalRequest.amount" required>
 
-                <label>Current Amount (How much do you have so far)</label>
-                <input type="number" v-model="GoalRequest.currentAmount" required>
-                
-                <div class="date">
-                <label >Deadline</label>
-                <input type="date" v-model="GoalRequest.deadline" required>
+                    <label>Current Amount (How much do you have so far)</label>
+                    <input type="number" v-model="GoalRequest.currentAmount" required>
+
+                    <div class="date">
+                        <label>Deadline</label>
+                        <input type="date" v-model="GoalRequest.deadline" required>
+                    </div>
+
+
+
+                    <h4>{{ message }}</h4>
+
+                    <div class="clearfix">
+                        <button type="button" class="signupbtn" @click="createGoal"> Create Goal </button>
+                        <button type="button" class="cancelbtn" @click="showGoals = !showGoals"> View current goals</button>
+                    </div>
+
+
                 </div>
-                
-                
-                
+            </form>
+        </div>
+    </section>
 
-                <div class="clearfix">
-                    <button type="button" class="signupbtn" @click="createGoal"> Create Goal </button>
-                    <button type="button" class="cancelbtn" @click="showGoals = !showGoals"> View current goals</button>
-                </div>
-
-
-            </div>
-        </form>
-    </div>
-    <div>
+    <aside>
         <!--show if the button is clicked-->
         <viewGoals v-if="showGoals" />
-    </div>
+
+    </aside>
 </template>
   
 <script>
@@ -56,7 +62,7 @@ export default {
     data() {
         return {
             customer: null,
-            GoalRequest: { name: '', amount: null, currentAmount: null, description: '' },
+            GoalRequest: { name: '', amount: null, currentAmount: null, description: '', deadline: '' },
             message: '',
             showGoals: false,
         }
@@ -64,17 +70,29 @@ export default {
     methods: {
         createGoal() {
             const customerId = localStorage.getItem('cid')
-            GoalService.createGoal(customerId, this.GoalRequest)
-                .then(response => {       // HttpStatus.OK
-                    var goal = response.data;
-                    console.log(goal)
-                    this.message = `Goal created succesfully.`
-                })
-                .catch(e => {
-                    this.message = e.response.data.message;
-                    console.log(e.response.data);
-                });
+            if (this.GoalRequest.name != '' && this.GoalRequest.amount != null && this.GoalRequest.deadline != '') {
+                GoalService.createGoal(customerId, this.GoalRequest)
+                    .then(response => {       // HttpStatus.OK
+                        var goal = response.data;
+                        console.log(goal)
+                        this.message = "New Financial Goal has been created. You got this!"
+                        //Refresh the data after success
+                        this.GoalRequest = { name: '', amount: null, currentAmount: null, description: '', deadline: '' }
+                    })
+                    .catch(e => {
+                        this.message = e.response.data.message;
+                        console.log(e.response.data);
+                        //Refresh the data in the form
+                        this.GoalRequest = { name: '', amount: null, currentAmount: null, description: '', deadline: '' }
+                    });
+            }else {
+                this.message = "Fill out the data first!"
+            }
+           // Hide Goal List
+           this.showGoals = false
+
         },
+
 
     },
     mounted() {
@@ -88,6 +106,15 @@ export default {
     padding-bottom: 5%;
 
 }
+
+aside {
+    padding: 3%;
+}
+h4{
+    padding-top: 2%;
+    padding-bottom: 2%;
+}
+
 .createGoal {
     float: left;
     width: 50%;
--- FrontEnd/vuefinance/src/components/ViewGoals.vue
+++ FrontEnd/vuefinance/src/components/ViewGoals.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <h1> Select a Goal to edit</h1>
         <table class="table">
             <thead>
                 <tr>
@@ -8,6 +9,7 @@
                     <th>Current Amount</th>
                     <th> Progress </th>
                     <th>Deadline</th>
+                    <th>Edit/Delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -16,7 +18,8 @@
                     <td>{{ goal.amount }}</td>
                     <td>{{ goal.currentAmount }}</td>
                     <td>{{(goal.currentAmount*100/goal.amount).toFixed(2)}} %</td>
-                    <td>{{ goal.deadline }}</td>
+                    <td>{{ goal.deadline }} </td>
+                    <td><span id =edit-icon v-on:click="editgoal(goal.id)">  📝</span><span v-on:click="confirmation(goal.id)" id=delete-icon>  ❌</span></td>
                 </tr>
             </tbody>
         </table>
@@ -24,6 +27,7 @@
 </template>
 
 <script>
+
 import GoalService from '../services/GoalService'
 export default {
 
@@ -34,7 +38,7 @@ export default {
             goals: [],
             currentGoal: null,
             currentIndex: -1,
-            title: ""
+            message:"",
 
         };
     },
@@ -46,7 +50,7 @@ export default {
                     {
                         this.goals = response.data.goal;
                         console.log(response.data.goal);
-                        
+
                     }
                 })
                 .catch(e => {
@@ -58,13 +62,29 @@ export default {
         refreshList() {
 
             this.goalList();
-            this.currentTutorial = null;
+            this.currentGoal = null;
             this.currentIndex = -1;
         },
+
+        deletegoal(gid){
+            const customerId = localStorage.getItem('cid')
+            GoalService.delete(customerId, gid)
+            this.refreshList()
+        },
+        confirmation(gid){
+            let text = "Are you sure you want to delete this Goal?"
+            if(confirm(text)==true){
+                this.deletegoal(gid)
+                alert("Goal has been deleted")
+                
+            }
+
+        }
 
     },
     mounted() {
         this.goalList();
+    
 }
 }
 
@@ -79,7 +99,9 @@ export default {
   border-collapse: collapse;
   width: 100%;
 }
-
+tr:hover {
+    background-color: #0080a7;
+}
 .table th,
 .table td {
   border: 1px solid #ddd;
@@ -91,4 +113,20 @@ export default {
   background-color: #00da80;
   color: #001e28;
 }
+
+td #edit-icon,
+td #delete-icon {
+  display: none;
+}
+
+tr:hover #edit-icon,
+tr:hover #delete-icon {
+  display: inline;
+  
+}
+#edit-icon:hover, #delete-icon:hover{
+    font-size: large;
+
+}
+
 </style>
