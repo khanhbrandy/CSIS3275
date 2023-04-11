f4df0be2eab132e0be0c5131c5219c84016cd779
akashbalyan <akash.balyan139600@gmail.com>
2023-04-10 18:08:17 -0700

    added instructions in diff languages

--- FinanceApp/src/main/java/com/example/FinanceApp/FinanceAppApplication.java
+++ FinanceApp/src/main/java/com/example/FinanceApp/FinanceAppApplication.java
@@ -106,32 +106,64 @@ public class FinanceAppApplication {
 			goalRepository.findAll().forEach(System.out::println);
 
 			// Adding instructions to the database
-			String engIns1 = "This is a demo instruction 1 in English";
-			String engIns2 = "This is a demo instruction 2 in English";
-			String engIns3 = "This is a demo instruction 3 in English";
+			String engIns1 = "1. The user needs to do registration first to use the application.";
+			String engIns2 = "2. On registration , the user must login with the username and password.";
+			String engIns3 = "3. To add a transaction you must click on transaction tab and fill all the requred fields .";
+			String engIns4 = "4. To set financial Goals, click on Goals and add required fields to add Goals.";
+			String engIns5 = "5. View Goals button help you seeing all the goals.";
+			String engIns6 = "6. Reminder section is to add reminders for future transactions. To add reminders you need to fill the required fields and add reminder.";
+			String engIns7 = "7. View Reminders button list down all the reminders which are pending or completed.";
+			String engIns8 = "8. Reports section is used to see your overall transactions summary, how much the amount have been credited or debited so far.It gives a graph representation of transaction summary.";
+			
+			String spaIns1 = "1. El usuario debe registrarse primero para usar la aplicación.";
+			String spaIns2 = "2. En el momento del registro, el usuario deberá iniciar sesión con el nombre de usuario y la contraseña.";
+			String spaIns3 = "3. Para agregar una transacción, debe hacer clic en la pestaña de transacción y completar todos los campos requeridos.";
+			String spaIns4 = "4. Para establecer objetivos financieros, haga clic en Objetivos y agregue los campos obligatorios para agregar objetivos.";
+			String spaIns5 = "5. El botón Ver objetivos lo ayuda a ver todos los objetivos.";
+			String spaIns6 = "6. La sección de recordatorios es para agregar recordatorios para transacciones futuras. Para agregar recordatorios, debe completar los campos obligatorios y agregar un recordatorio.";
+			String spaIns7 = "7. El botón Ver recordatorios enumera todos los recordatorios que están pendientes o completados.";
+			String spaIns8 = "8. La sección de informes se utiliza para ver el resumen general de sus transacciones, cuánto se ha acreditado o debitado hasta el momento. Ofrece una representación gráfica del resumen de la transacción.\n"
+					+ "";
 
-			String spaIns1 = "This is a demo instruction 1 in Spanish";
-			String spaIns2 = "This is a demo instruction 2 in Spanish";
-			String spaIns3 = "This is a demo instruction 3 in Spanish";
-
-			String freIns1 = "This is a demo instruction 1 in French";
-			String freIns2 = "This is a demo instruction 2 in French";
-			String freIns3 = "This is a demo instruction 3 in French";
+			String freIns1 = "1. L'utilisateur doit d'abord s'enregistrer pour utiliser l'application.";
+			String freIns2 = "2. Lors de l'inscription, l'utilisateur doit se connecter avec le nom d'utilisateur et le mot de passe.\n"
+					+ "";
+			String freIns3 = "3. Pour ajouter une transaction, vous devez cliquer sur l'onglet transaction et remplir tous les champs obligatoires.";
+			String freIns4 = "4. Pour définir des objectifs financiers, cliquez sur Objectifs et ajoutez les champs requis pour ajouter des objectifs.";
+			String freIns5 = "5. Le bouton Afficher les objectifs vous aide à voir tous les objectifs.";
+			String freIns6 = "6. Reminder permet d'ajouter des rappels pour les transactions futures. Pour ajouter des rappels, vous devez remplir les champs obligatoires et ajouter un rappel.";
+			String freIns7 = "7. Le bouton Afficher les rappels répertorie tous les rappels en attente ou terminés.";
+			String freIns8 = "8. La section Rapports est utilisée pour voir le résumé global de vos transactions, combien le montant a été crédité ou débité jusqu'à présent. Il donne une représentation graphique du résumé des transactions.";
 
 			ArrayList<String> instructions1 = new ArrayList<String>();
 			instructions1.add(engIns1);
 			instructions1.add(engIns2);
 			instructions1.add(engIns3);
+			instructions1.add(engIns4);
+			instructions1.add(engIns5);
+			instructions1.add(engIns6);
+			instructions1.add(engIns7);
+			instructions1.add(engIns8);
 
 			ArrayList<String> instructions2 = new ArrayList<String>();
 			instructions2.add(spaIns1);
-			instructions2.add(spaIns1);
-			instructions2.add(spaIns1);
+			instructions2.add(spaIns2);
+			instructions2.add(spaIns3);
+			instructions2.add(spaIns4);
+			instructions2.add(spaIns5);
+			instructions2.add(spaIns6);
+			instructions2.add(spaIns7);
+			instructions2.add(spaIns8);
 
 			ArrayList<String> instructions3 = new ArrayList<String>();
 			instructions3.add(freIns1);
-			instructions3.add(freIns1);
-			instructions3.add(freIns1);
+			instructions3.add(freIns2);
+			instructions3.add(freIns3);
+			instructions3.add(freIns4);
+			instructions3.add(freIns5);
+			instructions3.add(freIns6);
+			instructions3.add(freIns7);
+			instructions3.add(freIns8);
 
 			Instruction instruction1 = new Instruction(instructions1, "english");
 			Instruction instruction2 = new Instruction(instructions2, "spanish");
